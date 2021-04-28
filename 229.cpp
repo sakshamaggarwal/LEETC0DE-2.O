b6f61e04d3d633ebb610120e3ed9b2a1c8eeed19
SAKSHAM <mani.saksham12@gmail.com>
2021-04-29 02:35:27 +0530

    Majority Element II
    Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
    
    Follow-up: Could you solve the problem in linear time and in O(1) space.

--- 229.cpp
+++ 229.cpp
@@ -24,3 +24,28 @@ public:
         return ans;
     }
 };
+
+// Using Maps
+
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+        
+//         int n=nums.size();
+//         map<int,int> m;
+//         vector<int> ans;
+//         for(int i=0;i<n;i++)
+//         {
+//             m[nums[i]]++;
+//         }
+//         for(int i=0;i<n;i++)
+//         {
+//             if(m[nums[i]]>n/3)
+//             {
+//                 ans.push_back(nums[i]);
+//                 m[nums[i]]=0;
+//             }
+//         }
+//         return ans;
+//     }
+// };
