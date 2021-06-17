44fc1fc343ed887b6282018c8c76f84323ec1b37
SAKSHAM <mani.saksham12@gmail.com>
2021-06-17 16:59:16 +0530

    Intersection of Two Arrays II
    Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.

--- 350.cpp
+++ 350.cpp
@@ -1,24 +1,63 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-     
-        vector<int> v;
-        map<int,int> m1,m2;
         
-        for(int i: nums1)
-            m1[i]++;
-        for(int i: nums2)
-            m2[i]++;
-      
-        for(int i: nums1)
+        int n1=nums1.size(),n2=nums2.size();
+        
+        if (n1>n2)
         {
-            if(m1[i]!=0&&m2[i]!=0)
-            { 
-                v.push_back(i);
-                m1[i]--;
-                m2[i]--;
+            return intersect(nums2, nums1);
+        }
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        vector<int> v;
+        int i=0,j=0;
+        while(i<n1&&j<n2)
+        {
+            if(nums1[i]>nums2[j])
+                j++;
+            else if(nums1[i]<nums2[j])
+                i++;
+            else
+            {
+                v.push_back(nums1[i]);
+                i++;
+                j++;
             }
         }
         return v;
+        
     }
 };
+
+// MAP
+
+// class Solution {
+// public:
+//     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+     
+//         if (nums1.size() > nums2.size())
+//         {
+//             return intersect(nums2, nums1);
+//         }
+        
+//         vector<int> v;
+//         map<int,int> m1,m2;
+        
+//         for(int i: nums1)
+//             m1[i]++;
+//         for(int i: nums2)
+//             m2[i]++;
+      
+//         for(int i: nums1)
+//         {
+//             if(m1[i]!=0&&m2[i]!=0)
+//             { 
+//                 v.push_back(i);
+//                 m1[i]--;
+//                 m2[i]--;
+//             }
+//         }
+//         return v;
+//     }
+// };
