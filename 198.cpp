c9bcc55a570827dd232357a1385c6274ff1ad107
SAKSHAM <mani.saksham12@gmail.com>
2021-10-09 18:53:07 -0700

    House Robber
    You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
    Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.

---
+++ 198.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0)
+            return 0;
+        if(n==1)
+            return nums[0];
+        nums[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++)
+            nums[i]=max(nums[i]+nums[i-2],nums[i-1]);
+        return nums[n-1];
+    }
+};
+
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         if(n==0)
+//             return 0;
+//         if(n==1)
+//             return nums[0];
+//         nums[1]=max(nums[0],nums[1]);
+//         int ans=nums[1];
+//         for(int i=2;i<n;i++)
+//         {
+//             ans=max(nums[i-1],nums[i]+nums[i-2]);
+//             nums[i]=ans;
+//         }
+//         return ans;
+//     }
+// };
