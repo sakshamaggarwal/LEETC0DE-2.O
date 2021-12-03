292f8e4937aa2ee1f06d2aac9e2241f167aee3dd
SAKSHAM <mani.saksham12@gmail.com>
2021-12-03 15:55:28 -0800

    Jump Game
    You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
    Return true if you can reach the last index, or false otherwise.

---
+++ 55.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int j=n-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]+i>=j)
+                j=i;
+        }
+        return j==0;
+    }
+};
