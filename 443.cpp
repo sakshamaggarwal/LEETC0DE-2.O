5285b6f4e035ae52276c71d080baf0714adbc215
SAKSHAM <sakshaa1@uci.edu>
2022-11-06 14:46:23 -0800

    Update 443.cpp

--- 443.cpp
+++ 443.cpp
@@ -1,5 +1,17 @@
 class Solution {
 public:
+    string toStr(int num)
+    {
+        string ans="";
+        while(num)
+        {
+            int a=num%10;
+            ans+=a+'0';
+            num/=10;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
     int compress(vector<char>& chars) {
         int n=chars.size();
         int j=0,temp=0;
@@ -11,7 +23,7 @@ public:
             chars[temp++]=chars[i];
             if(j-i>1)
             {
-                string s=to_string(j-i);
+                string s=toStr(j-i);        //to_string(j-i)
                 for(char c:s)
                     chars[temp++]=c;
             }
