fcc450611d37ec4543571b5af8695610929488b4
SAKSHAM <mani.saksham12@gmail.com>
2024-02-28 23:53:11 -0800

    Replace Elements with Greatest Element on Right Side
    Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
    
    After doing so, return the array.
    
     
    
    Example 1:
    
    Input: arr = [17,18,5,4,6,1]
    Output: [18,6,6,6,1,-1]
    Explanation: 
    - index 0 --> the greatest element to the right of index 0 is index 1 (18).
    - index 1 --> the greatest element to the right of index 1 is index 4 (6).
    - index 2 --> the greatest element to the right of index 2 is index 4 (6).
    - index 3 --> the greatest element to the right of index 3 is index 4 (6).
    - index 4 --> the greatest element to the right of index 4 is index 5 (1).
    - index 5 --> there are no elements to the right of index 5, so we put -1.
    Example 2:
    
    Input: arr = [400]
    Output: [-1]
    Explanation: There are no elements to the right of index 0.
     
    
    Constraints:
    
    1 <= arr.length <= 104
    1 <= arr[i] <= 105

---
+++ 1299.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int n=arr.size();
+        for(int i=0;i<n-1;++i)
+        {
+            int maxi=0;
+            for(int j=i+1;j<n;++j)
+            {
+                maxi=max(maxi,arr[j]);
+            }
+            arr[i]=maxi;
+        }
+        arr[n-1]=-1;
+        return arr;
+    }
+};
